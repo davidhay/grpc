9e3889d3f0ef1687723181ff3d8dcd6fd42b33dc
David Hay <david.j.hay@gmail.com>
2022-02-22 12:19:14 +0000

    added GreetingClient code

--- src/main/java/com/ealanta/grpc/greeting/client/GreetingClient.java
+++ src/main/java/com/ealanta/grpc/greeting/client/GreetingClient.java
@@ -2,6 +2,12 @@ package com.ealanta.grpc.greeting.client;
 
 import com.proto.dummy.DummyMessage;
 import com.proto.dummy.DummyServiceGrpc;
+import com.proto.greet.GreetRequest;
+import com.proto.greet.GreetResponse;
+import com.proto.greet.GreetServiceGrpc;
+import com.proto.greet.GreetServiceGrpc.GreetServiceStub;
+import com.proto.greet.Greeting;
+import com.proto.greet.GreetingOrBuilder;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 
@@ -14,12 +20,18 @@ public class GreetingClient {
         .usePlaintext() //de-activates SSL (old?)
         .build();
 
-    DummyServiceGrpc.DummyServiceBlockingStub syncClient = DummyServiceGrpc.newBlockingStub(channel);
+    //DummyServiceGrpc.DummyServiceBlockingStub syncClient = DummyServiceGrpc.newBlockingStub(channel);
 
     //DummyServiceGrpc.DummyServiceFutureStub asyncClient = DummyServiceGrpc.newFutureStub(channel);
 
     //do something
     //syncClient.
+
+    GreetServiceGrpc.GreetServiceBlockingStub greetClient = GreetServiceGrpc.newBlockingStub(channel);
+    Greeting greeting = Greeting.newBuilder().setFirstName("Mickey").setLastName("Mouse").build();
+    GreetRequest greetingRequest = GreetRequest.newBuilder().setGreeting(greeting).build();
+    GreetResponse response = greetClient.greet(greetingRequest);
+    System.out.printf("RESPONSE [%s]%n",response.getResult());
 
     System.out.println("Shutting down channel");
     channel.shutdown();
