c297cdc328429facb54f8d25fafaccfb8860ef6b
David Hay <david.j.hay@gmail.com>
2022-02-22 14:29:52 +0000

    added another test

--- src/test/java/com/ealanta/grpc/prime/calculator/PrimeFactorCalculatorTest.java
+++ src/test/java/com/ealanta/grpc/prime/calculator/PrimeFactorCalculatorTest.java
@@ -2,6 +2,7 @@ package com.ealanta.grpc.prime.calculator;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
+import java.util.Arrays;
 import org.junit.jupiter.api.Test;
 
 public class PrimeFactorCalculatorTest {
@@ -9,6 +10,7 @@ public class PrimeFactorCalculatorTest {
 
   @Test
   public void testFactors(){
-      assertArrayEquals(new int[]{2,2,2,3,5}, PrimeFactorCalculator.primeFactors(120));
+    assertArrayEquals(new int[]{2,2,2,3,5}, PrimeFactorCalculator.primeFactors(120));
+    assertArrayEquals(new int[]{2, 5, 109, 521}, PrimeFactorCalculator.primeFactors(567890));
   }
 }
